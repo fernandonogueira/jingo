362b54666539df6006c18c65d47fc4e428932f2b
Fernando Nogueira <fernandojnn@gmail.com>
2017-03-07 02:55:20 -0300

    Improving tests

--- jingo-maps/src/test/java/jingo/maps/utils/TestUtils.java
+++ jingo-maps/src/test/java/jingo/maps/utils/TestUtils.java
@@ -4,6 +4,12 @@ public class TestUtils {
 
     public static String getTestKey() {
         String key = System.getProperty("JINGO_TEST_KEY");
+
+        if (key == null || key.isEmpty()) {
+            // verify if property is being passed by env vars
+            key = System.getenv("JINGO_TEST_KEY");
+        }
+        
         if (key == null || key.isEmpty()) {
             throw new RuntimeException("Could not start tests because JINGO_TEST_KEY is not set. " +
                     "Set it passing it as an environment variable. (-DJINGO_TEST_KEY=yourKey)");
